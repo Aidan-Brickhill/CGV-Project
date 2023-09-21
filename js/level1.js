0d96262dce5615da553f68f9737d865bbfb33bb5
Aidan <2427688@students.wits.ac.za>
2023-09-21 16:35:08 +0200

    Physics Stuff

--- js/level1.js
+++ js/level1.js
@@ -1,60 +1,23 @@
 import * as THREE from 'three';
 import {Box} from './aircraft.js';
-import {Ring} from './ring.js';
+import * as CANNON from 'cannon-es'
+import CannonDebugger from 'cannon-es-debugger';
 
 
-//setup and move the camera
 const gameCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 gameCamera.position.set(7.99, 3.89, 17.28);
 
 const gameScene = new THREE.Scene();
 
-//aircraft
-const aircraft = new Box({
-    width: 1,
-    height: 1,
-    depth: 1,
-    hexColour: 0xCD7DE1,
-    velocity: {
-        x: 0,
-        y: -0.01,
-        z: 0
-    },
-    position: {
-        x: 0,
-        y: 0,
-        z: 5
-    }
+const physicsWorld = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.82, 0),
 });
-aircraft.castShadow = true;
-gameScene.add(aircraft)
 
+let aircraftBody;
+let aircraft;
 
-// const aircraft2 = new Box({
-//     width: 1,
-//     height: 1,
-//     depth: 1,
-//     hexColour: 0xCD7fff,
-// });
-// aircraft2.castShadow = true
-// physics.add.exisisting(aircraft2);
-// gameScene.add(aircraft2)
-
-//ground
-const ground = new Box({
-    width: 100,
-    height: 0.25,
-    depth: 100,
-    hexColour: 0xCDE17D,
-    position: {
-        x: 0,
-        y: -10,
-        z: 0
-    }
-});
-ground.receiveShadow = true;
-gameScene.add(ground);
-
+let groundBody;
+let ground;
 //light sources - can play around here
 const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.z = 20;
@@ -64,4 +27,60 @@ gameScene.add(light);
 
 gameScene.add(new THREE.AmbientLight(0xffffff, 0.3))
 
-export {gameScene, gameCamera, aircraft, ground}
+async function initialize() {
+    //setup and move the camera
+   
+
+    // create a ground body with a static plane
+    //aircraft
+    aircraftBody = new CANNON.Body({
+    mass: 5,
+    shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)),
+    });
+    aircraftBody.position.set(0, 0, 5);
+    physicsWorld.addBody(aircraftBody);
+     aircraft = new Box({
+        width: 1,
+        height: 1,
+        depth: 1,
+        hexColour: 0xCD7DE1,
+        position: {
+            x: 0,
+            y: 0,
+            z: 5
+        }
+    });
+    aircraft.castShadow = true;
+    gameScene.add(aircraft)
+
+    //ground
+    groundBody = new CANNON.Body({
+        type: CANNON.Body.STATIC,
+        // infinte geometric plane
+        shape: new CANNON.Plane(),
+    });
+    // rotate ground body by 90 degrees
+    groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    groundBody.position.set(0,-10,0);
+    physicsWorld.addBody(groundBody);
+        
+
+    ground = new Box({
+        width: 100,
+        height: 0.25,
+        depth: 100,
+        hexColour: 0xCDE17D,
+        position: {
+            x: 0,
+            y: -10,
+            z: 0
+        }
+    });
+    ground.receiveShadow = true;
+    gameScene.add(ground);
+}
+
+initialize();
+
+
+export {gameScene, gameCamera, physicsWorld, aircraft, aircraftBody, ground, groundBody}
--- main.js
+++ main.js
@@ -1,27 +1,22 @@
 import * as THREE from 'three';
-// import { Project, Scene3D, PhysicsLoader } from 'enable3d'
+import * as CANNON from 'cannon-es'
+import CannonDebugger from 'cannon-es-debugger';
+
+import {Box} from './js/aircraft.js';
+import {Ring} from './js/ring.js';
 
 //for dev purposes (allows you to navigate the 3D space)
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import {Box} from './js/aircraft';
-import {Ring} from './js/ring.js';
-
-// import the main menu scene code from menu.js
-// import { createMainMenu } from './menu.js';
-
-// import {activeScene } from './menu.js';
-
-// Create the main menu using the imported function
 
 
 let MainMenu = true;
 let animationId;
-//array of all rings
+// //array of all rings
 const Rings = [];
 
-//check the collison between two boxes (returns true if they are touching)
-//need to create one between aircraft and gorund (once aircraft is no longer a box)
-//need one to create one between aircraft and rings to complete the purpose of the game
+// //check the collison between two boxes (returns true if they are touching)
+// //need to create one between aircraft and gorund (once aircraft is no longer a box)
+// //need one to create one between aircraft and rings to complete the purpose of the game
 export function BoxCollision({
     box1,
     box2
@@ -34,21 +29,22 @@ export function BoxCollision({
     return xCollision && yCollision && zCollision
 }
 
-//renderer setup
+
+
+
+
+
+// renderer setup
 const renderer = new THREE.WebGLRenderer({
     alpha: true,
     antialias: true
 });
-
-//enable shadows
+ //enable shadows
 renderer.shadowMap.enabled = true;
 renderer.setPixelRatio(1);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//for dev purposes
-
-//setting the scene
 
 //struct to track key inputs
 const keys = {
@@ -71,75 +67,91 @@ const keys = {
 
 
 import {menuScene, menuCamera} from "./js/mainMenu.js";
-import {gameScene, gameCamera, aircraft, ground} from "./js/level1.js";
-
+import {gameScene, gameCamera, physicsWorld, aircraft, aircraftBody, ground, groundBody}from "./js/level1.js";
+const cannonDebugger = new CannonDebugger(gameScene, physicsWorld, {
+    // color: 0xff0000,
+  });
 const controls = new OrbitControls(gameCamera, renderer.domElement)
+
+   
 
 let frames = 0;
 let spawnRate = 200;
+
 function animate() {
 
-    animationId = requestAnimationFrame(animate);
 
     if (MainMenu) {
         renderer.render(menuScene, menuCamera); //This line loads the Main Menu as the active scene at first, active scene gets updated on click
 
     } else {
+       
+
         renderer.render(gameScene, gameCamera);
+        physicsWorld.fixedStep();
+        cannonDebugger.update();
+        aircraft.position.copy(aircraftBody.position);
+        aircraft.quaternion.copy(aircraftBody.quaternion);
+        ground.position.copy(groundBody.position);
+        // ground.quaternion = (0,0,0);
+
+        animationId = requestAnimationFrame(animate);
+        console.log(animationId)
+
 
         //depeinding on keys presses, chnage the velocity
         //limit frame rate so game is constabnt on all screens 
         // use link 
         // https://chriscourses.com/blog/standardize-your-javascript-games-framerate-for-different-monitors
-        aircraft.velocity.x = 0;
-        aircraft.velocity.z = 0;
-        aircraft.velocity.y += aircraft.gravity;
-        if (keys.a.pressed)
-            aircraft.velocity.x = -0.05;
-        if (keys.d.pressed)
-            aircraft.velocity.x = 0.05;
-        if (keys.w.pressed)
-            aircraft.velocity.z = -0.05;
-        if (keys.s.pressed)
-            aircraft.velocity.z = 0.05;
-        if (keys.space.pressed) {
-            aircraft.velocity.y = 0.05;
-        }
-        aircraft.update(ground);
-        //first person
-        // camera.position.set( aircraft.position.x, aircraft.position.y, aircraft.position.z + aircraft.depth/2);  
+        // aircraft.velocity.x = 0;
+        // aircraft.velocity.z = 0;
+        // aircraft.velocity.y += aircraft.gravity;
+        // if (keys.a.pressed)
+        //     aircraft.velocity.x = -0.05;
+        // if (keys.d.pressed)
+        //     aircraft.velocity.x = 0.05;
+        // if (keys.w.pressed)
+        //     aircraft.velocity.z = -0.05;
+        // if (keys.s.pressed)
+        //     aircraft.velocity.z = 0.05;
+        // if (keys.space.pressed) {
+        //     aircraft.velocity.y = 0.05;
+        // }
+        // aircraft.update(ground);
+        // //first person
+        // // camera.position.set( aircraft.position.x, aircraft.position.y, aircraft.position.z + aircraft.depth/2);  
 
-        Rings.forEach((ring) => {
-            ring.update();
-            /*CHECK COLLSION between aircraft and ring
-            if collsion call
-            cancelAnimationFrame(animationID)
-            */
-        })
+        // Rings.forEach((ring) => {
+        //     ring.update();
+        //     /*CHECK COLLSION between aircraft and ring
+        //     if collsion call
+        //     cancelAnimationFrame(animationID)
+        //     */
+        // })
 
-        if (frames % spawnRate == 0) {
-            const ring = new Ring({
-                ringRadius: 2,
-                tubeRadius: 0.125,
-                hexColour: 0xFFD700,
-                position: {
-                    x: (Math.random() - 0.5) * 7,
-                    y: (Math.random() - 0.5) * 7,
-                    z: -10
-                },
-                velocity: {
-                    x: 0,
-                    y: 0,
-                    z: 0.05
-                }
-            });
-            ring.castShadow = true;
-            gameScene.add(ring);
-            Rings.push(ring);
-        }
+        // if (frames % spawnRate == 0) {
+        //     const ring = new Ring({
+        //         ringRadius: 2,
+        //         tubeRadius: 0.125,
+        //         hexColour: 0xFFD700,
+        //         position: {
+        //             x: (Math.random() - 0.5) * 7,
+        //             y: (Math.random() - 0.5) * 7,
+        //             z: -10
+        //         },
+        //         velocity: {
+        //             x: 0,
+        //             y: 0,
+        //             z: 0.05
+        //         }
+        //     });
+        //     ring.castShadow = true;
+        //     gameScene.add(ring);
+        //     Rings.push(ring);
+        // }
 
-        frames += 1;
-        //find camer postion
+        // frames += 1;
+        // //find camer postion
         // console.log(camera.position);S
     }
     //find camer postion
@@ -219,3 +231,7 @@ window.addEventListener('keyup', (event) => {
             break;
     }
 })
+
+
+
+
