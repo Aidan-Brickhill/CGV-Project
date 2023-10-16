d5ac6fde7ba5e13800517274599d0cd2fc6e23f1
gabrieldrew <100573616+gabrieldrew@users.noreply.github.com>
2023-10-16 11:14:09 +0200

    sinwave river level 1
    added sinwave/sigmoid river

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ document.body.appendChild(renderer.domElement);
 
 //imports from other levels
 import { menuScene, menuCamera, buttonScene, deathScene } from "./js/mainMenu.js";
-import { level1Scene, level1Camera, level1PhysicsWorld, level1Aircraft, level1AircraftBody,  level1MixerAircraft, level1Start, level1End} from "./js/level1.js";
+import { level1Scene, level1Camera, level1PhysicsWorld, level1Aircraft, level1AircraftBody,  level1MixerAircraft, level1Start, level1End, MAX_HEIGHT} from "./js/level1.js";
 import { level2Scene, level2Camera, level2PhysicsWorld, level2Aircraft, level2AircraftBody, level2MixerAircraft, level2Start, level2End , Rings} from "./js/level2.js";
 import { level3Scene, level3Camera, level3PhysicsWorld, level3Aircraft, level3AircraftBody, level3MixerAircraft, level3Start, level3End } from "./js/level3.js";
 
@@ -85,7 +85,7 @@ function animate(){
         let vxf = 0;
         let vyf = 0;
 
-        const ceiling1 = 30;
+        const ceiling1 = MAX_HEIGHT/2;
         const ceiling2 = 0;
         const ceiling3 = 0;
 
@@ -206,9 +206,9 @@ function animate(){
         // debug (allows you to see cannon bodies)
         cannonDebugger.update();
 
-        Rings.forEach((ring) => {            
-                checkRingCollision(aircraftBody.position, ring)
-        });
+        // Rings.forEach((ring) => {            
+        //         checkRingCollision(aircraftBody.position, ring)
+        // });
         //renders the scene
         if (dead){
             renderer.render(gameScene, perspectiveCamera);
@@ -382,7 +382,7 @@ function initializeLevel1Scene() {
     levelStart = level1Start;
     levelEnd = level1End;  
     physicsWorld.gravity.set(0,-1,0);
-    aircraftBody.position.set(0, 30, levelStart.y);
+    // aircraftBody.position.set(0, 30, levelStart.y);
     aircraftBody.velocity.set(0, 0, 0); // Set to zero to stop any motion
     aircraftBody.angularVelocity.set(0, 0, 0);
     aircraftBody.quaternion.setFromEuler(0, 0, 0);
